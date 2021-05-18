4429427f438ae77f91e0cd68d4d576b22c4c4f4d
Daniel Piron <dannypiron@gmail.com>
2021-05-18 09:46:30 -0400

    440hz sine tone using render logic

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CWARNINGS=-Wall -Wextra -Wshadow -Wnon-virtual-dtor -Wcast-align -Wunused -Woverloaded-virtual -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2
-CFLAGS=$(CWARNINGS) -Werror
+CFLAGS=$(CWARNINGS) -Werror -std=c++14 -fsanitize=address
 
 a.out: src/paex_sine_c++.cpp vendors/portaudio/include/portaudio.h
 	c++ $(CFLAGS) -Ivendors/portaudio/include/ -Lvendors/portaudio/lib/.libs -lportaudio src/paex_sine_c++.cpp
--- src/paex_sine_c++.cpp
+++ src/paex_sine_c++.cpp
@@ -1,6 +1,55 @@
 #include <portaudio.h>
 
+#include <cmath>
 #include <iostream>
+#include <vector>
+
+class Sample {
+public:
+    float operator[](const size_t index) const {
+        return _samples[index];
+    }
+    float operator[](const float index) const {
+        auto iIndex = static_cast<size_t>(floor(index));
+        float t = index - iIndex;
+        float v0 = _samples[iIndex];
+        float v1 = iIndex == (_samples.size() - 1) ? _samples[0] : _samples[iIndex + 1];
+        return v0 + t * (v1 - v0); 
+    }
+    size_t length() const { return _samples.size(); }
+
+public:
+    std::vector<float> _samples;
+};
+
+struct Channel {
+
+    Channel(const Sample* sample_ = nullptr) : sample(sample_)
+                                             , index(0)
+                                             , rate(1.0f) {}
+
+    void render(float* outputBuffer, unsigned long framesPerBuffer) {
+        if (sample == nullptr) return;
+        while (framesPerBuffer) {
+            size_t samplesUntilLoop = sample->length() - static_cast<size_t>(index);
+            bool loopOccurs = samplesUntilLoop < framesPerBuffer;
+            size_t renderPassLength = loopOccurs ? samplesUntilLoop : framesPerBuffer;
+
+            framesPerBuffer -= renderPassLength;
+            while (renderPassLength--) {
+                *outputBuffer++ = (*sample)[index];
+                index += rate;
+            }
+
+            if (loopOccurs) {
+                index -= sample->length();
+            }
+        }
+    }
+    const Sample* sample;
+    float index;
+    float rate;
+};
 
 static int patestCallback(const void *inputBuffer, void *outputBuffer,
                           unsigned long framesPerBuffer,
@@ -11,12 +60,11 @@ static int patestCallback(const void *inputBuffer, void *outputBuffer,
     (void)inputBuffer;
     (void)timeInfo;
     (void)statusFlags;
-    (void)userData;
 
+    auto channel = reinterpret_cast<Channel*>(userData);
     float* pOut = reinterpret_cast<float*>(outputBuffer);
-    while (framesPerBuffer--) {
-        *pOut++ = 0;
-    }
+    channel->render(pOut, framesPerBuffer);
+
     return paContinue;
 }
 
@@ -40,10 +88,18 @@ int main() {
         std::cerr << "Error: No default output device" << std::endl;
         return 1;
     }
-    outputParameters.channelCount = 2;
+    outputParameters.channelCount = 1;
     outputParameters.sampleFormat = paFloat32;
     outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
     outputParameters.hostApiSpecificStreamInfo = NULL;
+
+
+    Sample sample;
+    size_t sampleCount = 44100 / 440;
+    for (size_t i = 0; i < sampleCount; ++i) {
+        sample._samples.push_back(static_cast<float>(sin(M_PI * 2 * i / sampleCount)));
+    }
+    Channel channel(&sample);
 
     PaStream* stream = nullptr;
     err = Pa_OpenStream(
@@ -54,7 +110,7 @@ int main() {
         paFramesPerBufferUnspecified,
         paClipOff,
         patestCallback,
-        NULL);
+        reinterpret_cast<void*>(&channel));
     if (err != paNoError) {
         std::cerr << "Error opening stream" << std::endl;
         return 1;
